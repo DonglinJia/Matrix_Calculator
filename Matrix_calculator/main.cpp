7abca9ab6698a1773a0099f7a7bc866ab121a701
Leo Jia <Leo@v1020-wn-180-63.campus-dynamic.uwaterloo.ca>
2018-03-19 14:57:06 -0400

    Initial Commit

---
+++ Matrix_calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix_calculator
+//
+//  Created by Leo Jia on 2018-03-19.
+//  Copyright © 2018 Leo Jia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
